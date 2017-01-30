1584aef08e92a38f71d239f742dce07c401c2e7e
ChloeThiebaut <chloe.thiebaut76@gmail.com>
2017-01-30 18:06:54 +0100

    Update script-detail.js

--- ajax/tp4/script-detail.js
+++ ajax/tp4/script-detail.js
@@ -2,4 +2,11 @@ $(function(){
 
     var id_film = window.location.hash.substr(1);
     console.log(id_film);
+    
+     $.get("https://api.themoviedb.org/3/movie/ID_FILM?api_key=58f8fe741b03b0ae4c9a2ed080e94041", function(resultat){
+      console.log(resultat);
+  }); 
+   
+    
+    
 });
