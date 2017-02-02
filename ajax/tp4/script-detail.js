1126758412470c394aefff2d1946c8c0a0ac9772
ChloeThiebaut <chloe.thiebaut76@gmail.com>
2017-02-03 00:57:01 +0100

    Add files via upload

--- ajax/tp4/script-detail.js
+++ ajax/tp4/script-detail.js
@@ -1,12 +1,42 @@
 $(function(){
+    var id = window.location.hash.substr(1);
 
-    var id_film = window.location.hash.substr(1);
-    console.log(id_film);
-    
-     $.get("https://api.themoviedb.org/3/movie/ID_FILM?api_key=58f8fe741b03b0ae4c9a2ed080e94041", function(resultat){
-      console.log(resultat);
-  }); 
-   
-    
-    
+    $.get("https://api.themoviedb.org/3/movie/"+id+"?language=fr&api_key=58f8fe741b03b0ae4c9a2ed080e94041",function(movie){
+
+        // affichage des infos
+        $('.title').html(movie.title);
+        $('.affiche').attr("src","http://image.tmdb.org/t/p/w500/"+movie.poster_path);
+        $('.affiche').attr("alt",movie.title);
+        $('.synopsis').html(movie.overview);
+        $("<li>").text("Budget : "+movie.budget+" $").appendTo(".detailsFilm");
+
+        // affichage des genres
+        var genreGeneral=movie.genres.length;
+        var genres="";
+
+        for (i=0; i<genreGeneral; i++) {
+            genres=genres+movie.genres[i].name;
+            if(i!=genreGeneral-1){
+                genres=genres+", ";
+            }
+        }
+        $("<li>").text("Genres : "+genres).appendTo(".detailsFilm");
+
+        //affichage producteurs
+        var producGeneral=movie.production_companies.length;
+        var producteurs="";
+
+        for (i=0; i<producGeneral; i++) {
+            producteurs=producteurs+movie.production_companies[i].name;
+            if(i!=producGeneral-1){
+                producteurs=producteurs+", ";
+            }
+        }
+        $("<li>").text("Producteurs : "+producteurs).appendTo(".detailsFilm");
+        $("<li>").text("Langue originale : "+movie.original_language).appendTo(".detailsFilm");
+        $("<li>").text("Titre original : "+movie.original_title).appendTo(".detailsFilm");
+        $("<li>").text("Date de sortie : "+movie.release_date).appendTo(".detailsFilm");
+        $("<li>").text("Note : "+movie.vote_average).appendTo(".detailsFilm");
+
+    });
 });
--- ajax/tp4/script.js
+++ ajax/tp4/script.js
@@ -1,7 +1,44 @@
-$(function(){
+$(document).ready(function(){ 
 
-  $.get("http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=58f8fe741b03b0ae4c9a2ed080e94041", function(resultat){
-      console.log(resultat);
-  });
-    
+    $(function(){
+
+        var api_key = "58f8fe741b03b0ae4c9a2ed080e94041";
+        var id= window.location.hash.substr(1);
+        console.log(id);
+
+        // fonction recherche (faut appuyer sur entrée)
+        $("#recherche").change(function(){
+            var search = $(this).val();
+            $.get("http://api.themoviedb.org/3/search/movie?query=" + search + "&language=fr&api_key=" +api_key, function(results){
+                renderMovies(results);
+            });	
+        });
+
+        // affichage de la liste
+        var renderMovies = function(movies){
+            $("#listeFilmsIndex").html("");
+
+            for(var i = 0; i < movies.results.length; i++)
+            {
+               var movie = movies.results[i];
+                $("<div>").attr("class","filmIndex filmIndex"+i).appendTo("#listeFilmsIndex");
+                $("<div>").attr("class","filmImageIndex filmImageIndex"+i).appendTo(".filmIndex"+i);
+                $("<a>").attr("href","detail.html#"+movie.id).html("<img src='http://image.tmdb.org/t/p/w185/"+movie.poster_path+"' alt='poster'>").appendTo(".filmImageIndex"+i);
+                $("<div>").attr("class","detailsFilmIndex detailsFilmIndex"+i).appendTo(".filmIndex"+i);
+                $("<a>").attr("href","detail.html#"+movie.id).attr("class","titreFilmIndex").text(movie.title).appendTo(".detailsFilmIndex"+i);
+                $("<p>").text(movie.overview).attr("class","synopsisIndex").appendTo(".detailsFilmIndex"+i);
+            }
+        };
+
+        $.get("http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&language=fr&api_key=" +api_key,             function(results){
+            renderMovies(results);
+        });
+
+
+
+    });
+
 });
+
+
+
